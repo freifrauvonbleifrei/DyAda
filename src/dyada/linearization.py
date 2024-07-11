8e6b4fae1dc349be814bb5e3adb7dfb7d3b5cbd4
Freifrau von Bleifrei <freifrauvonbleifrei@users.noreply.github.com>
2024-07-11 10:22:17 +0900

    linearization: add missing space

--- src/dyada/linearization.py
+++ src/dyada/linearization.py
@@ -22,7 +22,7 @@ class MortonOrderLinearization(Linearization):
         assert this_level_increment.count() > 0
         index_in_box = history_of_indices[-1]
         if not index_in_box < 2 ** this_level_increment.count() or index_in_box < 0:
-            raise IndexError("Index " + str(index_in_box) +" out of bounds")
+            raise IndexError("Index " + str(index_in_box) + " out of bounds")
         
         number_of_dimensions = len(this_level_increment)
         binary_position = ba.bitarray(number_of_dimensions)
