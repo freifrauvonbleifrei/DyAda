50380bfc8734d319585af757a2a63074c6d9ce42
Freifrau von Bleifrei <freifrauvonbleifrei@gmx.net>
2024-07-11 13:56:17 +0900

    refinement: remove return type bracket annotation for python 3.8 compatibility

--- src/dyada/refinement.py
+++ src/dyada/refinement.py
@@ -101,7 +101,7 @@ class RefinementDescriptor:
         level_increment: ba.frozenbitarray
         count_to_go_up: int
 
-    def get_branch(self, index: int) -> deque[LevelCounter]:
+    def get_branch(self, index: int) -> deque:
         if index < 0 or index >= len(self):
             raise IndexError("Index out of range")
 
