coordinates: fix + test for underflow/overflow problems

src/dyada/coordinates.py
@@ -58,9 +58,14 @@ def get_coordinates_from_level_index(level_index: LevelIndex) -> CoordinateInter
     if (
         any(level_index.d_level < 0)
         or any(level_index.d_index < 0)
-        or any(level_index.d_index >= 2**level_index.d_level)
+        or any(level_index.d_index >= 2 ** np.array(level_index.d_level, dtype=int))
     ):
-        raise ValueError
+        error_string = "Invalid level index: {}".format(level_index)
+        if any(level_index.d_index >= 2 ** np.array(level_index.d_level, dtype=int)):
+            error_string += " (index {} too large, should be < {})".format(
+                level_index.d_index, 2 ** np.array(level_index.d_level, dtype=int)
+            )
+        raise ValueError(error_string)
     get_d_array = lambda x: np.fromiter(
         x,
         dtype=np.float32,
@@ -69,13 +74,13 @@ def get_coordinates_from_level_index(level_index: LevelIndex) -> CoordinateInter
     return CoordinateInterval(
         get_d_array(
             (
-                2 ** float(-l) * i
+                2.0 ** -float(l) * i
                 for l, i in zip(level_index.d_level, level_index.d_index)
             )
         ),
         get_d_array(
             (
-                2 ** float(-l) * (i + 1)
+                2.0 ** -float(l) * (i + 1)
                 for l, i in zip(level_index.d_level, level_index.d_index)
             )
         ),