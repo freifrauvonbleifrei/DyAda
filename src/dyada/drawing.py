3e12068f68e76f7a006f6f446889a6ef8f86de55
Freifrau von Bleifrei <freifrauvonbleifrei@gmx.net>
2024-09-03 13:38:10 +0900

    drawing: initial versions of tikz plotting w/ tests and (maybe) latexmk building in action

--- src/dyada/drawing.py
+++ src/dyada/drawing.py
@@ -5,9 +5,12 @@ try:
 except ImportError:
     warnings.warn("matplotlib not found, plotting functions will not work")
 
+from pathlib import Path
 from typing import Sequence, Union, Mapping, Optional
+import subprocess
 
 from dyada.coordinates import CoordinateInterval, get_coordinates_from_level_index
+from dyada.descriptor import branch_generator
 from dyada.structure import depends_on_optional
 
 
@@ -25,6 +28,45 @@ def plot_boxes_2d(
         return plot_boxes_2d_matplotlib(intervals, labels, projection, **kwargs)
     else:
         raise ValueError(f"Unknown backend: {backend}")
+
+
+def plot_all_boxes_2d(
+    refinement, projection: Sequence[int] = [0, 1], labels="patches", **kwargs
+) -> None:
+    level_indices = list(refinement.get_all_boxes_level_indices())
+    coordinates = [get_coordinates_from_level_index(box_li) for box_li in level_indices]
+    if labels == "patches":
+        labels = []
+        for i in range(len(refinement._descriptor)):
+            if refinement._descriptor.is_box(i):
+                labels.append(str(i))
+    if labels == "boxes":
+        labels = None
+    plot_boxes_2d(coordinates, projection=projection, labels=labels, **kwargs)
+
+
+def plot_boxes_3d(
+    intervals: Union[Sequence[CoordinateInterval], Mapping[CoordinateInterval, str]],
+    labels: Optional[Sequence[str]] = None,
+    projection: Sequence[int] = [0, 1, 2],
+    backend: str = "tikz",
+    **kwargs,
+) -> None:
+    assert len(projection) == 3
+    if labels is None:
+        labels = [str(i) for i in range(len(intervals))]
+    if backend == "tikz":
+        return plot_boxes_3d_tikz(intervals, labels, projection, **kwargs)
+    else:
+        raise ValueError(f"Unknown backend: {backend}")
+
+
+def plot_all_boxes_3d(
+    refinement, projection: Sequence[int] = [0, 1, 2], **kwargs
+) -> None:
+    level_indices = list(refinement.get_all_boxes_level_indices())
+    coordinates = [get_coordinates_from_level_index(box_li) for box_li in level_indices]
+    plot_boxes_3d(coordinates, projection=projection, **kwargs)
 
 
 @depends_on_optional("matplotlib.pyplot")
@@ -68,7 +110,273 @@ def plot_boxes_2d_matplotlib(
     plt.show()
 
 
-def plot_all_boxes_2d(refinement, projection: Sequence[int] = [0, 1], **kwargs) -> None:
-    level_indices = list(refinement.get_all_boxes_level_indices())
-    coordinates = [get_coordinates_from_level_index(box_li) for box_li in level_indices]
-    plot_boxes_2d(coordinates, projection=projection, **kwargs)
+def latex_write_and_compile(latex_string: str, filename: str) -> None:
+    dirname = Path.cwd()
+    with open(dirname / filename, "w") as f:
+        f.write(latex_string)
+
+    # if in environment, run pdflatex to generate pdf
+    # this needs `latexmk`, `pdflatex`, and `tikz` to be installed, e.g.
+    # through `apt install texlive-latex-extra latexmk`
+    try:
+        subprocess.run(
+            ["latexmk", "-interaction=nonstopmode", "-pdf", filename],
+            check=True,
+            cwd=dirname,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+        subprocess.run(
+            ["latexmk", "-c"],
+            check=False,
+            cwd=dirname,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+    except (subprocess.CalledProcessError, FileNotFoundError) as e:
+        warnings.warn(f"Error while running latexmk on {filename}: {e}")
+
+
+def latex_add_color_defs(
+    tikz_string: str, num_colors: int, colormap_name="viridis"
+) -> str:
+    cm = plt.get_cmap(colormap_name)
+    for leaf in range(num_colors):
+        colormap = cm(leaf * 1.0 / num_colors)
+        color = [int(255 * c) for c in colormap[:3]]
+
+        color_str = "color_%d" % leaf
+        tikz_string += "\\definecolor{%s}{RGB}{%d,%d,%d}\n" % (
+            color_str,
+            color[0],
+            color[1],
+            color[2],
+        )
+    return tikz_string
+
+
+# inspired by @griegler : https://github.com/griegler/octnet/issues/28
+def plot_boxes_3d_tikz(
+    intervals: Union[Sequence[CoordinateInterval], Mapping[CoordinateInterval, str]],
+    labels: Optional[Sequence[str]],
+    projection: Sequence[int],  # TODO use
+    wireframe: bool = False,
+    filename: Optional[str] = None,
+    **kwargs,
+) -> None:
+
+    def tikz_cube(interval: CoordinateInterval, option_string="") -> str:
+        tikz_string = ""
+        line_string = "\\draw[%s] (%f,%f,%f) -- (%f,%f,%f) -- (%f,%f,%f) -- (%f,%f,%f) -- cycle;\n"
+        lower = interval[0]
+        upper = interval[1]
+        # iterate the six sides of the cube
+        # by always selecting four corners that have one coordinate in common
+        # TODO use iterator
+        tikz_string += line_string % (
+            option_string,
+            *lower,
+            lower[0],
+            upper[1],
+            lower[2],
+            upper[0],
+            upper[1],
+            lower[2],
+            upper[0],
+            lower[1],
+            lower[2],
+        )
+
+        tikz_string += line_string % (
+            option_string,
+            *lower,
+            lower[0],
+            lower[1],
+            upper[2],
+            lower[0],
+            upper[1],
+            upper[2],
+            lower[0],
+            upper[1],
+            lower[2],
+        )
+
+        tikz_string += line_string % (
+            option_string,
+            lower[0],
+            upper[1],
+            lower[2],
+            upper[0],
+            upper[1],
+            lower[2],
+            *upper,
+            lower[0],
+            upper[1],
+            upper[2],
+        )
+        tikz_string += line_string % (
+            option_string,
+            *lower,
+            upper[0],
+            lower[1],
+            lower[2],
+            upper[0],
+            lower[1],
+            upper[2],
+            lower[0],
+            lower[1],
+            upper[2],
+        )
+        tikz_string += line_string % (
+            option_string,
+            lower[0],
+            lower[1],
+            upper[2],
+            lower[0],
+            upper[1],
+            upper[2],
+            *upper,
+            upper[0],
+            lower[1],
+            upper[2],
+        )
+        tikz_string += line_string % (
+            option_string,
+            upper[0],
+            lower[1],
+            lower[2],
+            upper[0],
+            lower[1],
+            upper[2],
+            *upper,
+            upper[0],
+            upper[1],
+            lower[2],
+        )
+        return tikz_string
+
+    def tikz_grid(intervals, wireframe: bool):
+        tikz_string = R"""\documentclass{standalone}
+% inspired by @griegler : https://github.com/griegler/octnet/issues/28
+\usepackage{xcolor}
+\usepackage{tikz,tikz-3dplot}
+\begin{document}
+\tdplotsetmaincoords{50}{130}
+\begin{tikzpicture}[scale=1.0, tdplot_main_coords]"""
+        tikz_string = latex_add_color_defs(tikz_string, len(intervals))
+        for grid_idx, interval in enumerate(intervals):
+            color_str = "color_%d" % grid_idx
+            if wireframe:
+                option_string = "very thin, %s" % (color_str)
+            else:
+                option_string = "very thin, gray, fill=%s,fill opacity=0.3" % (
+                    color_str
+                )
+            tikz_string += tikz_cube(interval, option_string)
+
+        tikz_string += "\\end{tikzpicture}\n"
+        tikz_string += "\\end{document}\n"
+        return tikz_string
+
+    latex_string = tikz_grid(intervals, wireframe)
+    if filename is None:
+        filename = "tikz_cubes"
+    if wireframe:
+        filename += "_wireframe.tex"
+    else:
+        filename += "_solid.tex"
+    latex_write_and_compile(latex_string, filename)
+
+
+def plot_tree_tikz(refinement_descriptor, filename="pow2tree"):
+    tikz_string = R"""\documentclass{standalone}
+\usepackage{forest}
+\begin{document}
+% cf. https://tex.stackexchange.com/questions/332300/draw-lines-on-top-of-tikz-forest
+\forestset{%
+    declare keylist register={through},
+    through={},
+    tracing tree/.style={%
+    delay={%
+        for #1={%
+        if phantom={}{through+/.option=name},
+        }
+    },
+    before drawing tree={%
+        tikz+/.wrap pgfmath arg={%
+        \foreach \i [count=\j, remember=\i as \k] in {##1} \ifnum\j>1 \draw [densely dashed, ->] (\k.west) -- (\i.west)\fi;
+        }{(through)}
+    },
+    }
+}
+\tikzset{every label/.style={xshift=-2ex,yshift=-2ex , font=\footnotesize, text=red}}
+"""
+    n_leaves = refinement_descriptor.get_num_boxes()
+    tikz_string = latex_add_color_defs(tikz_string, n_leaves)
+
+    tikz_string += "\\begin{forest}\n     tracing tree=tree,\n"
+    d_zeros = "0" * refinement_descriptor.get_num_dimensions()
+    leaf_string = (
+        "[" + d_zeros + ",circle,draw,fill=color_%d,fill opacity=0.4,label=%d]\n"
+    )
+    last_indent = 0
+    tab = "    "
+    num_box = 0
+    num_patch = 0
+    for current_branch, refinement in branch_generator(refinement_descriptor):
+        current_indent = len(current_branch)
+        while current_indent < last_indent:
+            tikz_string += tab * current_indent + "]\n"
+            last_indent -= 1
+        if refinement.count() == 0:
+            tikz_string += tab * current_indent + leaf_string % (num_box, num_patch)
+            num_box += 1
+        else:
+            tikz_string += (
+                tab * current_indent
+                + "["
+                + refinement.to01()
+                + ",label="
+                + str(num_patch)
+                + "\n"
+            )
+        last_indent = current_indent
+        num_patch += 1
+
+    tikz_string += "]" * (last_indent)
+    tikz_string += "\n\\end{forest}\n\\end{document}"
+
+    if not filename.endswith(".tex"):
+        filename += ".tex"
+    latex_write_and_compile(tikz_string, filename)
+
+
+def plot_descriptor_tikz(refinement_descriptor, filename="descriptor"):
+    tikz_string = R"""\documentclass{standalone}
+\usepackage{tikz}
+\usetikzlibrary{matrix}
+\begin{document}"""
+    num_colors = refinement_descriptor.get_num_boxes()
+    tikz_string = latex_add_color_defs(tikz_string, num_colors)
+    tikz_string += R"""\begin{tikzpicture}[every node/.style={draw,align=center,text height=2ex,minimum width=2ex, inner sep=0.2ex, fill opacity=0.4, text opacity=1}]
+    \matrix [draw=none, matrix of nodes,nodes in empty cells]
+    {"""
+    tab = "    "
+    box_counter = 0
+    for refinement in refinement_descriptor:
+        tikz_string += tab
+        if refinement.count() == 0:
+            tikz_string += f"|[fill=color_{box_counter}]| "
+            box_counter += 1
+        tikz_string += refinement.to01() + "&\n"
+
+    # remove last newline and ampersand
+    tikz_string = tikz_string[:-2]
+    tikz_string += R"""\\
+    };   
+\end{tikzpicture}
+\end{document}
+"""
+    if not filename.endswith(".tex"):
+        filename += ".tex"
+    latex_write_and_compile(tikz_string, filename)
--- test/test_drawing.py
+++ test/test_drawing.py
@@ -1,3 +1,4 @@
+import bitarray as ba
 from itertools import permutations
 import matplotlib.pyplot as plt
 import pytest
@@ -6,9 +7,19 @@ from dyada.coordinates import (
     get_coordinates_from_level_index,
     level_index_from_sequence,
 )
-from dyada.drawing import plot_boxes_2d, plot_all_boxes_2d
+from dyada.descriptor import RefinementDescriptor, validate_descriptor
+from dyada.drawing import (
+    plot_boxes_2d,
+    plot_all_boxes_2d,
+    plot_all_boxes_3d,
+    plot_tree_tikz,
+    plot_descriptor_tikz,
+)
 from dyada.linearization import MortonOrderLinearization
-from dyada.refinement import Discretization, RefinementDescriptor
+from dyada.refinement import (
+    Discretization,
+    PlannedAdaptiveRefinement,
+)
 from dyada.structure import depends_on_optional
 
 
@@ -55,3 +66,39 @@ def test_plot_boxes_2d_from_descriptor():
             # (because many boxes will be stacked on top of each other)
             # and nicer colors for the higher ones
             plot_all_boxes_2d(r, projection=list(projection), alpha=0.3)
+
+
+def test_plot_boxes_3d_from_descriptor():
+    descriptor = RefinementDescriptor(3, [1, 0, 1])
+    r = Discretization(MortonOrderLinearization(), descriptor)
+    p = PlannedAdaptiveRefinement(r)
+    p.plan_refinement(3, ba.bitarray("101"))
+    p.plan_refinement(1, ba.bitarray("001"))
+    p.plan_refinement(2, ba.bitarray("010"))
+    new_descriptor = p.apply_refinements()
+    validate_descriptor(new_descriptor)
+    r = Discretization(MortonOrderLinearization(), new_descriptor)
+    plot_all_boxes_3d(r, wireframe=False, draw_options="fill opacity=0.1")
+    plot_all_boxes_3d(r, wireframe=True, filename="test_filename")
+    plot_tree_tikz(new_descriptor)
+    plot_descriptor_tikz(new_descriptor)
+
+
+def test_plot_octree_3d_from_descriptor():
+    descriptor = RefinementDescriptor(3, [1, 1, 1])
+    r = Discretization(MortonOrderLinearization(), descriptor)
+    p = PlannedAdaptiveRefinement(r)
+    p.plan_refinement(4, ba.bitarray("111"))
+    p.plan_refinement(7, ba.bitarray("111"))
+    new_descriptor = p.apply_refinements()
+    validate_descriptor(new_descriptor)
+    r = Discretization(MortonOrderLinearization(), new_descriptor)
+    p = PlannedAdaptiveRefinement(r)
+    p.plan_refinement(new_descriptor.get_num_boxes() - 2, ba.bitarray("111"))
+    new_descriptor = p.apply_refinements()
+    r = Discretization(MortonOrderLinearization(), new_descriptor)
+    plot_all_boxes_3d(
+        r, wireframe=False, filename="octree", draw_options="fill opacity=0.1"
+    )
+    plot_all_boxes_3d(r, wireframe=True, filename="octree")
+    plot_tree_tikz(new_descriptor, filename="octree_tree")
