f704ba2c725620aa780da259d6d91e48d8562b50
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-06-25 15:19:04 +0900

    Revert "refinement: remove leftover caching refs"
    This reverts commit 7abd4da68498441d0064a033dbeaf14c04c32291.

--- src/dyada/refinement.py
+++ src/dyada/refinement.py
@@ -1,9 +1,10 @@
 import bitarray as ba
 from collections import defaultdict
+from functools import lru_cache
 import numpy as np
 import numpy.typing as npt
 from queue import PriorityQueue
-from typing import Optional, Union
+from typing import Optional, Sequence, Union
 
 from dyada.coordinates import bitarray_startswith
 from dyada.descriptor import (
@@ -16,6 +17,14 @@ from dyada.linearization import (
     get_dimensionwise_positions,
     get_dimensionwise_positions_from_branch,
 )
+
+
+def is_lru_cached(func):
+    while hasattr(func, "__wrapped__"):
+        if hasattr(func, "cache_info"):
+            return True
+        func = func.__wrapped__
+    return hasattr(func, "cache_info")
 
 
 class PlannedAdaptiveRefinement:
