8d80978a4d6a01484ee61e45f37dc8bb5c2bc6a5
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-08-05 16:27:34 +0900

    drawing: add alignment (but codes still not correct)

--- src/dyada/drawing.py
+++ src/dyada/drawing.py
@@ -660,7 +660,7 @@ def plot_location_stack_tikz(discretization: Discretization, filename="location_
     num_colors = descriptor.get_num_boxes()
     tikz_string = latex_add_color_defs(tikz_string, num_colors)
     tikz_string += R"""\begin{tikzpicture}[every node/.style={draw,align=center,text height=2ex,minimum width=2ex, inner sep=0.2ex, fill opacity=0.4, text opacity=1}]
-    \matrix [draw=none, matrix of nodes,nodes in empty cells]
+    \matrix [draw=none, matrix of nodes,nodes in empty cells, column 1/.style={nodes={anchor=east}}, column 2/.style={nodes={anchor=west}}]
     {""" #TODO left right alignment, monospace
     tab = "    "
     box_counter = 0
