139755ef9c2178b49fc98fb133fcfb8b40ea6e9b
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-11-27 16:59:52 +0900

    ancestrybranch: class to encapsulate iteration (joint iteration of old and under-construction descriptor)

--- src/dyada/refinement.py
+++ src/dyada/refinement.py
@@ -4,22 +4,17 @@ from functools import lru_cache
 import numpy as np
 import numpy.typing as npt
 from queue import PriorityQueue
+from types import MappingProxyType
 from typing import Optional, Union
 
-from dyada.coordinates import bitarray_startswith
+from dyada.ancestrybranch import AncestryBranch
 from dyada.descriptor import (
     RefinementDescriptor,
     get_regular_refined,
     hierarchical_to_box_index_mapping,
     find_uniqueness_violations,
-    Branch,
 )
 from dyada.discretization import Discretization
-from dyada.linearization import (
-    MortonOrderLinearization,
-    get_dimensionwise_positions,
-    get_dimensionwise_positions_from_branch,
-)
 
 
 def is_lru_cached(func):
@@ -227,227 +222,31 @@ class PlannedAdaptiveRefinement:
                 # no more indices left in markers
                 break
 
-    def refinement_with_marker_applied(
-        self, linear_index
-    ) -> tuple[ba.frozenbitarray, npt.NDArray[np.int8]]:
-        refinement = self._discretization.descriptor[linear_index]
-        marker = self._markers[linear_index]
-        if refinement.count() == 0:
-            assert np.all(marker > -1)
-            return refinement, marker
-
-        positive = ba.bitarray([1 if m > 0 else 0 for m in marker])
-        assert (refinement & positive).count() == 0
-        refinement |= positive
-
-        negative = ba.bitarray([1 if m < 0 else 0 for m in marker])
-        assert (~refinement & negative).count() == 0
-        refinement ^= negative
-
-        return refinement, marker
-
     def modified_branch_generator(self, starting_index: int):
         descriptor = self._discretization.descriptor
-        # iterates a modified version of the descriptor, incorporating the markers knowledge
-        # and keeping track of the ancestry
-        current_modified_branch, _ = descriptor.get_branch(
-            starting_index, is_box_index=False
-        )
-        initial_branch_depth = len(current_modified_branch)
-        history_of_indices, history_of_level_increments = (
-            current_modified_branch.to_history()
-        )
-        history_of_binary_positions = []
-        for i in range(initial_branch_depth - 1):
-            history_of_binary_positions.append(
-                self._discretization._linearization.get_binary_position_from_index(
-                    history_of_indices[: i + 1], history_of_level_increments[: i + 1]
-                )
-            )
-        # the ancestry, in old indices but new relatonships
-        ancestry = descriptor.get_ancestry(current_modified_branch)
-        assert len(ancestry) == initial_branch_depth - 1
+        ancestrybranch = AncestryBranch(self._discretization, starting_index)
+        proxy_markers = MappingProxyType(self._markers)
 
         intermediate_generation: list[int] = []
         while True:
-            # get the currently desired location info
-            modified_dimensionwise_positions = get_dimensionwise_positions(
-                history_of_binary_positions, history_of_level_increments
-            )
-            current_old_index, intermediate_generation = self.find_next_twig(
-                modified_dimensionwise_positions,
-                ancestry[-1] if len(ancestry) > 0 else 0,
-            )
-            ancestry.append(current_old_index)
-            next_refinement, next_marker = self.refinement_with_marker_applied(
-                current_old_index
+            current_old_index, intermediate_generation, next_refinement, next_marker = (
+                ancestrybranch.get_current_location_info(proxy_markers)
             )
             if next_refinement == descriptor.d_zeros:
                 yield current_old_index, next_refinement, next_marker
                 for p in intermediate_generation:
-                    yield p, ba.bitarray(None), ancestry[-2]
+                    yield p, ba.bitarray(None), ancestrybranch.ancestry[-2]
                 # only on leaves, we can advance the branch
                 try:
-                    current_modified_branch.advance_branch(initial_branch_depth)
+                    ancestrybranch.advance()
                 except IndexError:
                     # done!
                     return
-                # prune all other data to current length,
-                # so we don't have to recompute from new branch
-                current_modified_branch_depth = len(current_modified_branch)
-                history_of_binary_positions = history_of_binary_positions[
-                    : current_modified_branch_depth - 2
-                ]
-                history_of_indices = history_of_indices[
-                    : current_modified_branch_depth - 1
-                ]
-                history_of_indices[-1] += 1
-                history_of_level_increments = history_of_level_increments[
-                    : current_modified_branch_depth - 1
-                ]
-                ancestry = ancestry[: current_modified_branch_depth - 1]
+
             else:
                 # on non-leaves, we need to grow the branch
                 yield current_old_index, next_refinement
-                current_modified_branch.grow_branch(next_refinement)
-                history_of_level_increments.append(next_refinement)
-                history_of_indices.append(0)
-
-            # update history of binary positions
-            latest_binary_position = (
-                self._discretization._linearization.get_binary_position_from_index(
-                    history_of_indices, history_of_level_increments
-                )
-            )
-            history_of_binary_positions.append(latest_binary_position)
-
-    def is_old_index_now_at_or_containing_location_code(
-        self,
-        desired_dimensionwise_positions: list[ba.bitarray],
-        parent_of_next_refinement: int,
-        parent_branch: Branch,
-        old_index: int,
-    ) -> tuple[bool, list[ba.bitarray]]:
-        descriptor = self._discretization.descriptor
-        num_dimensions = descriptor.get_num_dimensions()
-        old_index_branch, _ = descriptor.get_branch(
-            old_index,
-            is_box_index=False,
-            hint_previous_branch=(parent_of_next_refinement, parent_branch),
-        )
-        old_index_dimensionwise_positions = get_dimensionwise_positions_from_branch(
-            old_index_branch, self._discretization._linearization
-        )
-
-        old_index_ancestors = descriptor.get_ancestry(old_index_branch)
-        old_index_ancestry_accumulated_markers = np.sum(
-            [self._markers[ancestor] for ancestor in old_index_ancestors],
-            axis=0,
-        )
-        shortened_parent_positions = [
-            desired_dimensionwise_positions[d][
-                : len(desired_dimensionwise_positions[d])
-                - old_index_ancestry_accumulated_markers[d]
-            ]
-            for d in range(num_dimensions)
-        ]
-        part_of_history = all(
-            bitarray_startswith(
-                old_index_dimensionwise_positions[d],
-                shortened_parent_positions[d],
-            )
-            for d in range(num_dimensions)
-        )
-        return part_of_history, old_index_dimensionwise_positions
-
-    def find_next_twig(
-        self,
-        desired_dimensionwise_positions: list[ba.bitarray],
-        parent_of_next_refinement: int,
-    ) -> tuple[int, list[int]]:
-        """get the (old) tree node corresponding to the location code, and any nodes encountered on the way
-        Args:
-            desired_dimensionwise_positions (list[ba.bitarray]): the location code we're looking for next
-            parent_of_next_refinement (int): parent or other ancestor of the index we're looking for
-        Returns:
-            tuple[int, list[int]]:
-                tuple consisting of the (old) node index
-                and a list of (otherwise forgotten) intermediate nodes:
-                    these intermediate nodes are descendants of parent_of_next_refinement,
-                    and either culled children of the returned node
-                    (in case it is now a leaf),
-                    or culled ancestors of the returned node
-        """
-        # with which next (old) index do we get the currently desired position?
-
-        descriptor = self._discretization.descriptor
-        if len(desired_dimensionwise_positions) == 0:
-            return 0, []  # root node
-        num_dimensions = descriptor.get_num_dimensions()
-        parent_branch, _ = descriptor.get_branch(
-            parent_of_next_refinement, is_box_index=False
-        )
-        children = descriptor.get_children(parent_of_next_refinement, parent_branch)
-
-        # determine which child twig to go down next,
-        # keeping track of predecessors that are going to disappear
-        twig_found = False
-        intermediate_generation: list[int] = []
-        while not twig_found:
-            # find the child whose branch puts it at the same level/index as
-            # the modified branch we're looking at
-            for child in children:
-                part_of_history, child_dimensionwise_positions = (
-                    self.is_old_index_now_at_or_containing_location_code(
-                        desired_dimensionwise_positions,
-                        parent_of_next_refinement,
-                        parent_branch,
-                        child,
-                    )
-                )
-
-                if not part_of_history:
-                    continue
-
-                child_future_refinement, child_marker = (
-                    self.refinement_with_marker_applied(child)
-                )
-                if (
-                    np.min(child_marker) >= 0
-                    or child_future_refinement != descriptor.d_zeros
-                ):
-                    return child, intermediate_generation
-
-                children_of_coarsened = descriptor.get_children(child)
-                history_matches = all(
-                    child_dimensionwise_positions[d]
-                    == desired_dimensionwise_positions[d]
-                    for d in range(num_dimensions)
-                )
-                # if it's a perfect match, it's a coarsened node
-                if history_matches:
-                    twig_found = True
-                    # this means that its former children are now gone
-                    # and need to be mapped to this child's index
-                    for child_of_coarsened in children_of_coarsened:
-                        # need to append the binarized index of the child, broadcast to split dimensions
-                        # this needs linearization (if not morton order)
-                        if not (
-                            self._discretization._linearization
-                            == MortonOrderLinearization()
-                        ):
-                            raise NotImplementedError(
-                                "Refinement tracking not implemented for non-Morton order linearizations"
-                            )
-                        intermediate_generation.append(child_of_coarsened)
-                    return child, intermediate_generation
-                else:
-                    # else, it's a node that's going to disappear
-                    # -> restart loop with new children and remember this one
-                    intermediate_generation.append(child)
-                    children = children_of_coarsened
-
-        raise RuntimeError("Logic error: no matching child found in find_next_twig.")
+                ancestrybranch.grow(next_refinement)
 
     def track_indices(self, old_index: int, new_index: int) -> None:
         assert new_index > -1
