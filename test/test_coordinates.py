5d087d53be8445ddd3be3f3917c73e05f49586bc
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-12-02 16:23:29 +0900

    coordinates: add & test a few conversion functions

--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -1,3 +1,4 @@
+import bitarray as ba
 import numpy as np
 import pytest
 from random import randint
@@ -5,11 +6,15 @@ from random import randint
 from dyada.coordinates import (
     DyadaTooFineError,
     LevelIndex,
+    bitarray_startswith,
     level_index_from_sequence,
     interval_from_sequences,
     get_coordinates_from_level_index,
     float_parts_bitarray,
     location_code_from_float,
+    location_code_from_coordinate,
+    location_code_from_level_index,
+    level_index_from_location_code,
 )
 
 
@@ -69,3 +74,37 @@ def test_float_parts_bitarray():
         float_parts_bitarray(np.float128(1.0))
     with pytest.raises(ValueError):
         float_parts_bitarray(np.float16(1.0))
+
+
+def test_location_code_from_coordinate():
+    coordinate = np.array([0.0, 0.25, 0.5, 0.75])
+    location_code = location_code_from_coordinate(coordinate)
+    expected_codes = [
+        ba.bitarray(""),  # 0.0
+        ba.bitarray("01"),  # 0.25
+        ba.bitarray("1"),  # 0.5
+        ba.bitarray("11"),  # 0.75
+    ]
+    for code, expected in zip(location_code, expected_codes):
+        assert bitarray_startswith(code, expected)
+
+
+def test_conversions_random_data():
+    assert len(ba.bitarray("0")) == 1
+    for _ in range(100):
+        num_dimensions = randint(1, 6)
+        level = [randint(0, 10) for _ in range(num_dimensions)]
+        index = [randint(0, 2**l_d - 1) for l_d in level]
+        level_index = level_index_from_sequence(level, index)
+        interval = get_coordinates_from_level_index(level_index)
+        coordinate_from_li = interval.lower_bound
+
+        location_code_from_c = location_code_from_coordinate(coordinate_from_li)
+        location_code_from_li = location_code_from_level_index(level_index)
+        for from_c, from_li in zip(location_code_from_c, location_code_from_li):
+            assert bitarray_startswith(from_c, from_li)
+        # coordinate_from_location_code = ...#TODO
+        # assert np.allclose(coordinate_from_li, coordinate_from_location_code)
+
+        level_index_from_lc = level_index_from_location_code(location_code_from_li)
+        assert level_index == level_index_from_lc
