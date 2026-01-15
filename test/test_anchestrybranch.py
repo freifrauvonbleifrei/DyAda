5c10f874e6783530c0359cef88cec419dca6cdaa
Theresa Pollinger <theresa.pollinger@riken.jp>
2026-01-15 11:14:38 +0900

    test branch generator: up to interesting part

--- test/test_anchestrybranch.py
+++ test/test_anchestrybranch.py
@@ -198,10 +198,20 @@ def test_modified_branch_generator():
             ba.bitarray("01 00 10 10 00 00 10 00 00"),
         ),
     )
+    assert (
+        discretization_to_2d_ascii(discretization, resolution=(8, 4))
+        == """\
+_________
+| | | | |
+|_|_|_|_|
+|       |
+|_______|"""
+    )
     p = PlannedAdaptiveRefinement(discretization)
     p._markers[0] = np.array([1, 0], dtype=np.int8)
     p._markers[6] = np.array([-1, 0], dtype=np.int8)
     new_descriptor = RefinementDescriptor(2)
+    new_descriptor._data = ba.bitarray()
     p._index_mapping = [set() for _ in range(len(discretization.descriptor))]
     # call like in add_refined_data
     generator = p.modified_branch_generator(starting_index=0)
@@ -215,6 +225,124 @@ def test_modified_branch_generator():
     assert p._index_mapping == [
         {0},
         *(set(), set(), set(), set(), set(), set(), set(), set()),
+    ]
+    first_refinement = next(generator)
+    assert first_refinement.type == p.Refinement.Type.ExpandLeaf
+    assert first_refinement.old_index == 1
+    assert first_refinement.new_refinement == ba.bitarray("00")
+    assert np.array_equal(
+        first_refinement.marker_or_ancestor, np.array([0, 0], dtype=np.int8)
+    )
+    p.extend_descriptor_and_track_indices(
+        new_descriptor,
+        first_refinement.old_index,
+        ba.bitarray("00"),
+    )
+    assert p._index_mapping == [
+        {0},
+        {1},
+        *(set(), set(), set(), set(), set(), set(), set()),
+    ]
+
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 1, None, 0)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1},
+        *(set(), set(), set(), set(), set(), set(), set()),
+    ]
+
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 1, None, 1)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1},
+        *(set(), set(), set(), set(), set(), set(), set()),
+    ]
+    second_refinement = next(generator)
+    assert second_refinement.type == p.Refinement.Type.ExpandLeaf
+    assert second_refinement.old_index == 1
+    assert second_refinement.new_refinement == ba.bitarray("00")
+    assert np.array_equal(
+        second_refinement.marker_or_ancestor, np.array([0, 0], dtype=np.int8)
+    )
+    p.extend_descriptor_and_track_indices(
+        new_descriptor,
+        first_refinement.old_index,
+        ba.bitarray("00"),
+    )
+
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 1, None, 0)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1, 2},
+        *(set(), set(), set(), set(), set(), set(), set()),
+    ]
+
+    third_refinement = next(generator)
+    assert third_refinement == p.Refinement(
+        p.Refinement.Type.CopyOver, 2, ba.bitarray("10"), None
+    )
+    p.extend_descriptor_and_track_indices(
+        new_descriptor,
+        third_refinement.old_index,
+        third_refinement.new_refinement,
+    )
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 2, None, 0)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1, 2},
+        {0, 3},
+        *(set(), set(), set(), set(), set(), set()),
+    ]
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 2, None, 3)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1, 2},
+        {0, 3},
+        *(set(), set(), set(), set(), set(), set()),
+    ]
+
+    fourth_refinement = next(generator)
+    assert fourth_refinement == p.Refinement(
+        p.Refinement.Type.CopyOver, 3, ba.bitarray("10"), None
+    )
+    p.extend_descriptor_and_track_indices(
+        new_descriptor,
+        fourth_refinement.old_index,
+        fourth_refinement.new_refinement,
+    )
+    assert new_descriptor._data == ba.bitarray("11 00 00 10 10")
+
+    tracking_refinement = next(generator)
+    assert tracking_refinement == p.Refinement(p.Refinement.Type.TrackOnly, 3, None, 3)
+    p.track_indices(
+        tracking_refinement.old_index, tracking_refinement.marker_or_ancestor
+    )
+    assert p._index_mapping == [
+        {0},
+        {0, 1, 2},
+        {0, 3},
+        {3, 4},
+        *(set(), set(), set(), set(), set()),
     ]
 
 
